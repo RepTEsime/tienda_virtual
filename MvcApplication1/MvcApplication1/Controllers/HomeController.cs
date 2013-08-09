e28a7fd82e84d30176c9034d16e9c159ea190f3c
Alberto Jimenez <alberto_bad@msn.com>
2013-08-09 02:08:59 -0500

    agregue

---
+++ MvcApplication1/MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "ASP.NET MVC";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
